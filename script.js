0907d421d126143bd5788175ddee4b6c329bbea0
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-10-02 23:00:55 -0700

    initial commit

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 
 const API_KEY = 'api_key=13b596630dc0d884b9c0c895ad80e11b';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'
-+API_KEY;
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'
+
 
 const main = document.getElementById('main');
-
+const form = document.getElementById('form');
+const search = document.getElementById ('search');
 
 getMovies(API_URL);
 
@@ -23,25 +25,24 @@ function showMovies(data) {
   main.innerHTML = '';
 
   data.forEach(movie => {
+    
     const {title, poster_path, vote_average, overview} = movie;
     const movieEl = document.createElement('div');
     movieEl.classList.add('movie');
     movieEl.innerHTML = `
-    
-    <img src="${IMG_URL+poster_path}" alt="${title}">
+          <img src="${IMG_URL+poster_path}" alt="${title}">
 
-    <div class="movie--info">
-      <h3>${title}</h3>
-      <span class="${getColor(vote_average)}">${vote_average}</span>
-    </div>
-    <div class="overview">
-      <h3>Overview</h3>
-      ${overview}
-    </div>
-    
-    `
+          <div class="movie--info">
+            <h3>${title}</h3>
+            <span class="${getColor(vote_average)}">${vote_average}</span>
+          </div>
+          <div class="overview">
+            <h3>Overview</h3>
+            ${overview}
+          </div>
 
-    main.apprendChild(movieEl);
+       `
+       main.appendChild(movieEl)
   })
 }
 
@@ -56,3 +57,13 @@ function getColor(vote) {
     return 'red'
   }
 }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if(searchTerm) {
+    getMovies()
+  }
+})

abfd962e695cdcecae7836d9e2e1d1f9aeb33f2c
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-10-02 23:00:46 -0700

    initial commit

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 const API_KEY = 'api_key=13b596630dc0d884b9c0c895ad80e11b';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'
++API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
 const main = document.getElementById('main');
